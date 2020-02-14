7d105ad5cf9682a9ea76bdb400edce3a23bcec51
Ken Shiraishi <kshiraishi22@gmail.com>
2020-02-14 01:18:23 -0500

    added api routes but urls are not working

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,12 +1,30 @@
+const db = require("../models");
+
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
-    burgers.selectAll(function(data) {
-      let hdbr_obj = {
-        burgers: data
-      }
-      console.log(hdbr_obj)
-      res.render("index", hdbr_obj)
+  app.get("/api/burgers", function(req, res) {
+    db.burgers.findAll({}).then(function(data) {
+      res.json(data)
+      console.log(data)
     });
-  })
+  });
+
+  app.post("/api/burgers", function(req, res) {
+    db.burgers.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }).then(function(response) {
+      res.json(response)
+    });
+  });
+
+  app.delete("/api/burgers/:id", function(req, res) {
+    db.burgers.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(response) {
+      res.json(response)
+    })
+  });
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -13,6 +14,7 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Sets up handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -22,9 +24,69 @@ app.use(express.static("public"));
 // Routes
 require("./routes/api_routes.js")(app);
 
+// Create connection with localhost
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html page with the plans in it.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { burgers: data });
+  });
+});
+
+// Create a new burger
+app.post("/api/burgers", function(req, res) {
+  connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new burger
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Devour (Delete) a burger
+app.delete("/api/burgers/:id", function(req, res) {
+  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
 // Starting our Express app
 db.sequelize.sync({ force: true }).then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+// Figure out how delete can allow for the text to move over to the other side.
