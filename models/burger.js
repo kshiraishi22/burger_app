ef38a9688cf3d591e8f139c4a029ab4a5d94a267
Ken Shiraishi <kshiraishi22@gmail.com>
2020-02-18 16:23:58 -0500

    error: class constructor model cannot be invoked without new

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,8 @@
 // Sequelize (capital) references the standard library
-const Sequelize = require("sequelize");
+let Sequelize = require("sequelize");
 
 // sequelize (lowercase) references our connection to the DB.
-const sequelize = require("../config/connection.js")
+let sequelize = require("../config/connection.js")
 
 // Creates a "Burgers" model that matches up with DB
 let Burger = sequelize.define("burger", {
