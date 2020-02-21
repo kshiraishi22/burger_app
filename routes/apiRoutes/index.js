118c774f59cef3bdee0eb165ce9e19204306775c
Ken Shiraishi <kshiraishi22@gmail.com>
2020-02-21 10:05:54 -0500

    styling successful, changed ref of css and image

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,24 +5,5 @@ router.use("/burger", burgerRoutes)
 
 module.exports = router;
 
-// module.exports = function(app) {
-//   // Get all burgers
-//   app.get("/", function(req, res) {
-//     db.Burger.findAll({}).then(function(data) {
-//       console.log(data)
-//       res.json(data);
-//   });
-// })
 
-//   // Add a burger
-//   app.post("/api/add", function(req, res) {
-//     console.log(req.body);
-//     db.Burger.create({
-//       burger_name: req.body.burger_name,
-//       devoured: req.body.devoured
-//     }).then(function(data) {
-//       res.json(data);
-//     });
-//   });
-// };
   
